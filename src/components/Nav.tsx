fa8186f41ebceefaf00a01cb25c7fa85a6b700e4
Luis Gonzalez <lgonzalez23@verizon.net>
2023-09-24 19:53:50 -0700

    navbar logo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import headerLogo from "../assets/images/header-logo.svg";
+import hamburger from "../assets/icons/hamburger.svg";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header className='padding-x'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href='/'>
+          <img
+            src={headerLogo}
+            alt='nike logo'
+            width={130}
+            height={29}
+          />
+        </a>
+        <img
+          src={hamburger}
+          alt='hamburger menu icon'
+          width={25}
+          height={25}
+          className='md:hidden'
+        />
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
