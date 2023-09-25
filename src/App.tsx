03becdcb368eff782cc8702dfd70d994986d8c81
Luis Gonzalez <lgonzalez23@verizon.net>
2023-09-24 19:17:37 -0700

    default styles removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,44 @@ import "./App.css";
 
 function App() {
   return (
-    <div className=''>
-      <h1 className='blue_gradient text-8xl'>Nike</h1>
-    </div>
+    <main className='relative'>
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        <h1 className='blue_gradient text-8xl'>
+          Hero Section
+        </h1>
+      </section>
+      <section className='padding'>
+        <h1 className='blue_gradient text-8xl'>
+          Popular Products
+        </h1>
+      </section>
+      <section className='padding'>
+        <h1 className='blue_gradient text-8xl'>
+          Super Quality
+        </h1>
+      </section>
+      <section className='padding-x py-10'>
+        <h1 className='blue_gradient text-8xl'>Services</h1>
+      </section>
+      <section className='padding'>
+        <h1 className='blue_gradient text-8xl'>
+          Special Offer
+        </h1>
+      </section>
+      <section className='padding bg-pale-blue'>
+        <h1 className='blue_gradient text-8xl'>
+          Customer Reviews
+        </h1>
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <h1 className='blue_gradient text-8xl'>
+          Subscribe
+        </h1>
+      </section>
+      <section className='padding bg-black padding-x padding-t pb-8'>
+        <h1 className='blue_gradient text-8xl'>Footer</h1>
+      </section>
+    </main>
   );
 }
 
