f2d3854cd02e7d4da8b663df73827d84dfe7564c
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-12 19:53:48 -0800

    heading styles

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
+import { statistics } from "../constants";
 
 const Hero = () => {
   return (
@@ -8,11 +9,17 @@ const Hero = () => {
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
       <div className='relative xl:w-2/5 w-full flex flex-col justify-center items-start max-xl:padding-x pt-28'>
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className='text-xl font-montserrat text-coral-red'>
+          Our Summer Collection
+        </p>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-6xl'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+            The New Arrival
+          </span>
           <br />
-          <span>Nike</span>
+          <span className='text-coral-red inline-block mt-3'>
+            Nike
+          </span>{" "}
           Shoes
         </h1>
         <p>
@@ -20,7 +27,16 @@ const Hero = () => {
           and innovation for you active needs.
         </p>
         <Button label='Show now' iconUrl={arrowRight} />
-        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'></div>
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          {statistics.map(({ label, value }) => {
+            return (
+              <div key={label}>
+                <p>{value}</p>
+                <p>{label}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
