79280f5e54e84f55eba5946556f4cb1cff057756
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-12 19:36:47 -0800

    applying Button

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,6 @@
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+
 const Hero = () => {
   return (
     <section
@@ -16,6 +19,8 @@ const Hero = () => {
           Discover stlyish Nike arrivals, quality, comfort,
           and innovation for you active needs.
         </p>
+        <Button label='Show now' iconUrl={arrowRight} />
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'></div>
       </div>
     </section>
   );
