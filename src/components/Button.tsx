fdf674f2e3defe75636a2d2693e6ffbf9df891eb
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-12 19:32:26 -0800

    button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,9 +10,16 @@ const Button = ({
   iconUrl = "/arrow-right.svg",
 }: ButtonProps) => {
   return (
-    <button type='button'>
+    <button
+      type='button'
+      className='flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'
+    >
       {label}
-      <img src={iconUrl} alt='button icon' />
+      <img
+        className='ml-2 rounded-full w-5 h-5'
+        src={iconUrl}
+        alt='button icon'
+      />
     </button>
   );
 };
