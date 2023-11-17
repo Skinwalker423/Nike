c803470fc8b0789e7c30df1d82b691c87b36e8a3
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-16 18:02:14 -0800

    scaling snimation on view

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,9 +1,18 @@
 import ServiceCard from "../components/ServiceCard";
 import { services } from "../constants";
+import { useRef } from "react";
+import { useIsVisible } from "../hooks/useIsVisible";
 
 const Services = () => {
+  const servicesRef = useRef(null);
+  const isVisible = useIsVisible(servicesRef);
   return (
-    <section className='max-container flex justify-center flex-wrap gap-9 transition-opacity ease-in opacity-100 hover:opacity-0  '>
+    <section
+      ref={servicesRef}
+      className={`max-container flex justify-center flex-wrap gap-9 transition-transform duration-300 ease-in ${
+        isVisible ? "scale-1" : "scale-0"
+      }`}
+    >
       {services.map((service) => {
         return (
           <ServiceCard key={service.label} {...service} />
