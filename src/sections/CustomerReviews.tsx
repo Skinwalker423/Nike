f75344acc78977a2b939b29aeaf3ec68f141d22e
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-17 17:04:46 -0800

    review card file

--- src/sections/CustomerReviews.tsx
+++ src/sections/CustomerReviews.tsx
@@ -1,3 +1,6 @@
+import ReviewCard from "../components/ReviewCard";
+import { reviews } from "../constants";
+
 const CustomerReviews = () => {
   return (
     <section className='max-container'>
@@ -12,7 +15,11 @@ const CustomerReviews = () => {
         Read genuine stories from our satisfied customers
         about their exceptional experiences with us
       </p>
-      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'></div>
+      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
+        {reviews.map((review) => {
+          return <ReviewCard key={review.customerName} />;
+        })}
+      </div>
     </section>
   );
 };
