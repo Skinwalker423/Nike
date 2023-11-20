75a53709641d0796b9125fec0c84f06547d339ee
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-19 17:50:33 -0800

    socials icons

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,4 +1,5 @@
 import { footerLogo } from "../assets/images";
+import { socialMedia } from "../constants";
 
 const Footer = () => {
   return (
@@ -14,11 +15,26 @@ const Footer = () => {
               height={46}
             />
           </a>
-          <p>
+          <p className='text-base leading-7 font-montserrat text-white-400 sm:max-w-sm'>
             Get shoes ready for the new term at your nearest
             Nike store. Find your perfect size in store. Get
             Rewards.
           </p>
+          <div className='flex items-center gap-5 mt-8'>
+            {socialMedia.map(({ src, alt }) => {
+              return (
+                <div className='flex justify-center items-center rounded-full bg-white-400 h-12 w-12'>
+                  <img
+                    src={src}
+                    alt={alt}
+                    width={24}
+                    height={24}
+                    className='object-contain'
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </footer>
